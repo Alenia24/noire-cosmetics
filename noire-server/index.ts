c79cbb8abc92a2e5811bfc698f9d24e3d5cabff6
Alenia24 <alenia.allenn@gmail.com>
2025-06-08 20:34:50 -0400

    Import database connection

--- noire-server/index.ts
+++ noire-server/index.ts
@@ -2,8 +2,14 @@ import express, { Request, Response} from "express";
 import { config } from "dotenv";
 config();
 
+// Import database connection
+import connectDB from "./database/database";
+
 const app = express();
 const PORT = process.env.PORT || 5050;
+
+//Invoke connectDB function to establish connection
+connectDB()
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Noire Cosmetics API.");
