15d44b2b7cfe85382571c0a08b337b5e11eff0cf
Alenia24 <alenia.allenn@gmail.com>
2025-06-08 21:16:03 -0400

    Import and set up orders,users router

--- noire-server/index.ts
+++ noire-server/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
 import { config } from "dotenv";
 config();
 
@@ -6,20 +6,24 @@ config();
 import connectDB from "./database/database";
 
 // Import routes
-import productsRouter from "./routes/products/products-route"
+import productsRouter from "./routes/products/products-route";
+import usersRouter from "./routes/users/users-route";
+import ordersRouter from "./routes/orders/orders-route";
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 //Invoke connectDB function to establish connection
-connectDB()
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Noire Cosmetics API.");
 });
 
 // Enable routes use
-app.use("/products", productsRouter)
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
+app.use("/users", usersRouter);
 
 // App listen
 app.listen(PORT, () => {
